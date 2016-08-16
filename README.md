eb572b10ef6323e763501ad7b0ed322e15c5b64d
zvolsky <zvolsky@seznam.cz>
2016-08-16 11:03:26 +0200

    readme, setup

--- README.md
+++ README.md
@@ -11,4 +11,6 @@ Make possible to read from dbf's with codepage unsupported by modules dbf and co
 	    print fix_895(record.autor)
 	t.close()
 
+You can ignore Python 2.7 installation error in test_v3.py (from aenum which is requirement of dbf).
+
 For 620 pl Mazovia: You can define conversion map in fix_620(). If possible please: Fork me, clone your fork, commit/push, generate Pull request from your fork. Thx.
--- setup.py
+++ setup.py
@@ -1,9 +1,35 @@
-from distutils.core import setup
+def read_md(f):   # as long PyPI doesn't support README.md
+    try:
+        from pypandoc import convert      # need (apt)pandoc + (pip)pypandoc
+        return convert(f, 'rst')
+    except ImportError:
+        return open(f, 'r').read()
+
+try:
+    from setuptools import setup
+    st = True
+except ImportError:
+    from distutils.core import setup
+    st = False
+
+if st:
+    try:
+        from setuptools.command.build_py import build_py_2to3 as build_py
+    except ImportError:
+        from setuptools.command.build_py import build_py
+else:
+    try:
+        from distutils.command.build_py import build_py_2to3 as build_py
+    except ImportError:
+        from distutils.command.build_py import build_py
+
 setup(
+  cmdclass={'build_py': build_py},
   name='dbf_read_iffy',
   py_modules=['dbf_read_iffy'],
   version='1.0',
   description="Read from dbf's if codepage is unsupported by modules dbf and codecs (895 cz Kamenicky, ..).",
+  long_description=read_md('README.md'),
   install_requires=['dbf'],
   author='Mirek Zvolsky',
   author_email='zvolsky@seznam.cz',
@@ -11,7 +37,7 @@ setup(
   download_url='https://github.com/zvolsky/dbf_read_iffy/tarball/1.0',
   keywords=['dbf', 'dbase', 'foxpro', 'vfp', '895', 'Kamenicky', '620', 'Mazovia', 'encoding'],
   classifiers=[
-      'Development Status :: 5 - Production/Stable',
+      'Development Status :: 4 - Beta',
       'Intended Audience :: Developers',
       'Intended Audience :: Information Technology',
       'License :: OSI Approved :: MIT License',
